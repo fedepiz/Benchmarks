69c70d6c7fc5b1d739aa7495bf7d7e9528da7586
Federico Pizzuti <pizzutifederico27@gmail.com>
2016-08-13 23:43:45 +0200

    Added square accs

---
+++ SquareAcc10000/Par/lift.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <malloc.h>
+#include <math.h>
+float squareAdd(float x, float y){
+  return x + (y * y);
+}
+float id(float x){
+  { return x; }
+}
+void liftKernel(float* v__10, float* v__16){ 
+{
+  /* Static local memory */
+  /* Typed Value memory */
+  float v__11;
+  /* Private Memory */
+  float v_tmp_18 = 0.0f;
+  v__11 = v_tmp_18;
+  /* reduce_par */
+  #pragma omp parallel for reduction (+:v__11)
+  for (int v_i_8 = 0;v_i_8<10000;v_i_8 = (1 + v_i_8)){
+    v__11 = squareAdd(v__11, v__10[v_i_8]);
+  }
+  /* end reduce_par */
+  /* map_seq */
+  /* iteration count is exactly 1, no loop emitted */
+  {
+    int v_i_9 = 0;
+    v__16[v_i_9] = id(v__11);
+  }
+  /* end map_seq */
+}}
+
+
+//Auto-generated runtime harness
+
+char *readFile() {
+  FILE* f = fopen("data.txt", "rb");
+  fseek(f, 0, SEEK_END);
+  long length = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  char* buffer = malloc(length);
+  if(buffer) {
+    fread(buffer, 1, length, f);
+  }
+  fclose(f);
+  return buffer;
+}
+    int main(int argc, char** argv) {
+int arrCount;
+char* inputData = strtok(readFile(),"@");
+//code for parameter p24
+float *p24 = malloc(sizeof(float) * 10000);
+//geerating array of type float
+for(unsigned long i1 = 0; i1 < 10000; i1++){
+inputData = strtok(NULL, "_"); sscanf(inputData,"%f",&(p24[(i1 * 10000/10000)]));
+}
+//main invocation
+float *output = malloc(sizeof(float) * 1);
+struct timeval startTime;
+gettimeofday(&startTime, NULL);
+liftKernel( p24, output);
+struct timeval endTime;gettimeofday(&endTime, NULL);
+float totalTime = ((endTime.tv_sec * 1e6 + endTime.tv_usec) - (startTime.tv_sec * 1e6 + startTime.tv_usec))/1000;
+printf("\n");
+printf("Elapsed time = %f\n",totalTime);
+}
---
+++ SquareAcc50000/Seq/lift.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <malloc.h>
+#include <math.h>
+float square(float x){
+  return x*x;
+}
+float add(float x, float y){
+  { return x+y; }
+}
+float id(float x){
+  { return x; }
+}
+void liftKernel(float* v__12, float* v__15, float* v__19){ 
+{
+  /* Static local memory */
+  /* Typed Value memory */
+  float v__13;
+  /* Private Memory */
+  float v_tmp_21 = 0.0f;
+  v__13 = v_tmp_21;
+  /* omp_map */
+  #pragma omp parallel for
+  for (int v_i_9 = 0;v_i_9<50000;v_i_9 = (1 + v_i_9)){
+    v__15[v_i_9] = square(v__12[v_i_9]);
+  }
+  /* end omp_map */
+  /* reduce_seq */
+  for (int v_i_10 = 0;v_i_10<50000;v_i_10 = (1 + v_i_10)){
+    v__13 = add(v__13, v__15[v_i_10]);
+  }
+  /* end reduce_seq */
+  /* map_seq */
+  /* iteration count is exactly 1, no loop emitted */
+  {
+    int v_i_11 = 0;
+    v__19[v_i_11] = id(v__13);
+  }
+  /* end map_seq */
+}}
+
+
+//Auto-generated runtime harness
+
+char *readFile() {
+  FILE* f = fopen("data.txt", "rb");
+  fseek(f, 0, SEEK_END);
+  long length = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  char* buffer = malloc(length);
+  if(buffer) {
+    fread(buffer, 1, length, f);
+  }
+  fclose(f);
+  return buffer;
+}
+    int main(int argc, char** argv) {
+int arrCount;
+char* inputData = strtok(readFile(),"@");
+//code for parameter p247
+float *p247 = malloc(sizeof(float) * 50000);
+//geerating array of type float
+for(unsigned long i1 = 0; i1 < 50000; i1++){
+inputData = strtok(NULL, "_"); sscanf(inputData,"%f",&(p247[(i1 * 50000/50000)]));
+}
+//Code for intermediate parameters
+float *v__15 = malloc(sizeof(float) * 50000);
+//main invocation
+float *output = malloc(sizeof(float) * 1);
+struct timeval startTime;
+gettimeofday(&startTime, NULL);
+liftKernel( p247, v__15, output);
+struct timeval endTime;gettimeofday(&endTime, NULL);
+float totalTime = ((endTime.tv_sec * 1e6 + endTime.tv_usec) - (startTime.tv_sec * 1e6 + startTime.tv_usec))/1000;
+printf("\n");
+printf("Elapsed time = %f\n",totalTime);
+}
